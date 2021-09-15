3814f951d5cc7ec90d88d15c9ad28ee2dd4fbb42
Elliot <elliot.widell@elev.ga.ntig.se>
2021-09-15 14:56:54 +0200

    2021-09-15 commit

--- tamagotchi/Program.cs
+++ tamagotchi/Program.cs
@@ -6,11 +6,26 @@ class Program
     {
 
         Tamagotchi tama = new Tamagotchi();
-        Console.WriteLine(":)");
-        tama.Hi();
-        tama.printStats();
-        tama.learnWord();
-        Console.ReadLine();
+        Console.WriteLine("Hello Please Name Your Tamagotchi: ");
+        tama.name = Console.ReadLine();
+        Console.WriteLine($"Great Your Tamagotchi Is Now Named {tama.name}");
+        Console.WriteLine($"You Can Interact With {tama.name} By Typing: Feed, Learn, Speak, Rest Or Print");
+        while(tama.getAlive())
+        {
+        tama.playerInput = Console.ReadLine().ToLower();
+        tama.actions();
+        
+
+        
+        
+        
+
+
+
+        }
+
+        
+        
 
 
     }
--- tamagotchi/Tamagotchi.cs
+++ tamagotchi/Tamagotchi.cs
@@ -9,8 +9,11 @@ public class Tamagotchi
     private bool Alive = true;
     public string name = "";
     private int needToPoop = 0;
+    public string playerInput = "";
 
-    private List<string> words = new List<string>() { "HeHe", "GonGongGingGang", "YoYoYo" };
+    private List<string> words = new List<string>() { 
+         "GonGongGingGang", "YoYoYo", "Just Woke Up, SWAG Is On"
+         };
 
     private Random generator = new Random();
 
@@ -26,12 +29,16 @@ public class Tamagotchi
         {
             Console.WriteLine("Not Hungry");
         }
+        tick();
+
+
     }
 
-    public void Hi()
+    public void hi()
     {
         int randomWordNumb = generator.Next(0, words.Count);
         Console.WriteLine(words[randomWordNumb]);
+        tick();
 
     }
 
@@ -40,27 +47,80 @@ public class Tamagotchi
     {
 
         words.Add(Console.ReadLine());
+        tick();
 
 
 
     }
 
-    public void tick()
-    {
-
-
-
-    }
+   
 
 
     public void printStats()
     {
 
-        Console.WriteLine("Alive: ", Alive);
-        Console.WriteLine("Boredom: ", boredom);
-        Console.WriteLine("Hunger: ", hunger);
-        Console.WriteLine("Need To Poop: ", needToPoop);
+        Console.WriteLine($"Alive: {Alive}");
+        Console.WriteLine($"Boredom: {boredom}");
+        Console.WriteLine($"Hunger: {hunger}");
+        Console.WriteLine($"Need To Poop: {needToPoop}");
+    }
+
+    public void tick()
+    {
+
+        boredom++;
+        hunger++;
+        needToPoop++;
+
+        
+    }
+
+    public bool getAlive()
+    {
+
+        return Alive;
+
+
     }
 
 
+
+    public void actions()
+    {
+    if(playerInput == "feed")
+    {
+        feed();
+
+    }
+
+    if(playerInput == "learn")
+    {
+        Console.WriteLine($"What Word Do You Want {name} To Learn?");
+        learnWord();
+        
+
+
+    }
+    if(playerInput == "speak")
+    {
+
+        hi();
+    }
+
+    if(playerInput == "rest")
+    {
+
+        needToPoop = needToPoop-2;
+    }
+    if(playerInput == "print")
+    {
+
+        printStats();
+    }
+
+    }
+
+
+
 }
+
